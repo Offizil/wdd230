68358e9124d850f90e876856d71f9b07d1d23354
Offizil <fredoffizil@gmail.com>
2024-02-21 16:46:41 +0100

    Chamber Home Page Build

--- chamber/scripts/menuscripts.js
+++ chamber/scripts/menuscripts.js
@@ -18,3 +18,41 @@ The CSS class rules will handle the different views, layouts, and displays.
 🗝️ JavaScript only applies the class value or not.
 */
 
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+
+
+// const content = document.querySelector("#content");
+// const allcontent = document.querySelectorAll('#content, #content *,  #header, #footer, #current_events, #spotlights_events, #weather_section, #forecast, #forecast *');
+
+
+const footer = document.querySelector("footer");
+const header = document.querySelector("header");
+const events = document.querySelector("#current_events");
+const advert = document.querySelector("#spotlights_adverts");
+const weather = document.querySelector("#weather_section");
+const forecast = document.querySelector(".forecast");
+// const arti
+
+
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+	// allcontent.classList.toggle('dark-mode');
+
+	footer.classList.toggle("dark-mode");
+	header.classList.toggle("dark-mode");
+	events.classList.toggle("dark-mode");
+	advert.classList.toggle("dark-mode");
+	weather.classList.toggle("dark-mode");
+	forecast.classList.toggle("dark-mode");
+	
+
+
+});
+
+
