e9b4b64786dc3df9bdc5e71ace6b48d034b8bfa6
Offizil <fredoffizil@gmail.com>
2024-03-03 16:55:08 +0100

    chamber discover page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -14,3 +14,48 @@
 
   // Update the content of the element
   lastModifiedElement.textContent = `Last modified: ${formattedDate} at ${formattedTime}`;
+
+
+
+
+  let welcomePara = document.querySelector("#welcome-card");
+
+  const msToDays = 8460000;
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Check if localStorage is supported by the browser
+    if (typeof(Storage) !== "undefined") {
+        // Get the current date and time
+        let currentDate =  new Date();
+
+        // Check if lastVisiitDate is stored in localStorage
+        if (localStorage.getItem("lastVisitDate") !== null) {
+            // Retrieve the last visit date from localStorage
+            let lastVisitDate = new Date(localStorage.getItem("lastVisitDate"));
+
+            // Calculate the time difference in milliseconds
+            let timeDifference = currentDate - lastVisitDate;
+            
+            // let timeDifference =  ( currentDate - lastVisitDate ) / msToDays;
+            
+            // Calculate the time difference in days
+            let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+            // Display appropriate message based on the time difference
+            if (daysDifference <1) {
+              welcomePara.textContent = "Back so soon! Awesome!";
+            } else {
+              welcomePara.textContent = `You last visited ${daysDifference} days ago.`;
+            }
+        } else {
+          // If it's the first visit, display a welcome message
+          welcomePara.textContent = "Welcome! Let us know if you have any questions.";
+        }
+
+        // Store the current date and time in localStorage
+        localStorage.setItem("lastVisitDate", currentDate.toString());
+    } else {
+        alert("Sorry, your browser does not support Web Storage. Last visit date cannot be stored.");
+    }
+});
+
