ce2152e37cd0ffb7e2723e9ef50e4b5ed5c41111
Offizil <fredoffizil@gmail.com>
2024-03-12 07:42:10 +0100

    updated errors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -19,3 +19,16 @@
   lastModifiedElement.textContent = `Last modified: ${formattedDate} at ${formattedTime}`;
 
 
+
+
+  // -----script for range functionality----
+  const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("page-rating");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
